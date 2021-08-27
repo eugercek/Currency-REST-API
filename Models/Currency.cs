616257c8835628899dd9296622fe25c77878f5a6
Emin Umut Gerçek <umutgercek1999@gmail.com>
2021-08-27 17:08:44 +0300

    Add demo model

---
+++ Models/Currency.cs
@@ -0,0 +1,9 @@
+namespace API.Models
+{
+    public class Currency
+    {
+        public string Name { get; set; }
+        public decimal BuyingPrice { get; set; }
+        public decimal SellingPrice { get; set; }
+    }
+}
